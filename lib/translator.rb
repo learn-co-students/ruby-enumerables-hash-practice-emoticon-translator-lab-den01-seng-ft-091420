31bcd68d189f1fcba33f2484a8b4a5e683d4dbc3
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-11 15:47:29 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,40 @@
+require 'yaml'
+require 'pry'
 # require modules here
 
-def load_library
-  # code goes here
+def load_library(path)
+  helper = YAML.load_file(path)
+  translated = {}
+  helper.each do |name, array|
+    translated[name] = {
+      :english => array[0],
+      :japanese => array[1]
+      # binding.pry
+    }
+    # binding.pry
+  end
+  translated
+  # binding.pry
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  helper = load_library(path)
+  helper.each do |eng, hash|
+    if hash[:english] == emoticon
+      return hash[:japanese]
+    end
+  end
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(path, emoticon)
+  helper = load_library(path)
+  helper.each do |eng, hash|
+    if hash[:japanese] == emoticon
+      return eng
+    end
+    # binding.pry
+  end
+  return "Sorry, that emoticon was not found"
 end
